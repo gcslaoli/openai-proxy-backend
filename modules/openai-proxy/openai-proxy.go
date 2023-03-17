074a24257201e77fb6483690bb0b88f02c210f83
LiDong <47240528+gcslaoli@users.noreply.github.com>
2023-03-17 10:23:35 +0800

    精简代码

--- modules/openai-proxy/openai-proxy.go
+++ modules/openai-proxy/openai-proxy.go
@@ -1,64 +1,16 @@
 package openai_proxy
 
 import (
-	"errors"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"openai-proxy-backend/modules/openai-proxy/config"
 	_ "openai-proxy-backend/modules/openai-proxy/controller"
 	_ "openai-proxy-backend/modules/openai-proxy/middleware"
 	"openai-proxy-backend/modules/openai-proxy/service"
 
 	"github.com/gogf/gf/v2/frame/g"
-	"github.com/gogf/gf/v2/net/ghttp"
-	"github.com/gogf/gf/v2/os/gctx"
 )
 
 func init() {
-	ctx := gctx.GetInitCtx()
-	OpenaiProxyKeysService := service.NewOpenaiProxyKeysService()
+
 	s := g.Server()
-	s.BindHandler("/v1/chat/completions", func(r *ghttp.Request) {
-		// 检查用户token
-		token := r.Request.Header.Get("Authorization")
-		if token != "Bearer "+config.Config.Token {
-			r.Response.WriteJson(g.Map{
-				"code":    1001,
-				"message": "Key错误",
-			})
-			return
-		}
 
-		key, err := OpenaiProxyKeysService.RondomGetKey(ctx)
-		if err != nil {
-			r.Response.WriteJson(g.Map{
-				"code":    1001,
-				"message": "无可用OpenAI Key",
-			})
-			return
-		}
-		u, _ := url.Parse(config.Config.OpenAIHost)
-		proxy := &httputil.ReverseProxy{Director: func(req *http.Request) {
-			req.URL.Scheme = u.Scheme
-			req.URL.Host = u.Host
-			req.Host = u.Host
-			req.Header.Set("Authorization", "Bearer "+key)
-		},
-			ModifyResponse: func(resp *http.Response) error {
-				if resp.StatusCode != http.StatusOK {
-					token := resp.Request.Header.Get("Authorization")
-					g.Dump(token)
-					g.Log().Errorf(ctx, "Current Key is %s,  OpenAI API error: %s", token, resp.Status)
-					// 设置requestKey失效 requestKey为token 去掉Bearer 后面的
-					requestKey := token[7:]
-					OpenaiProxyKeysService.SetKeyInvalid(ctx, requestKey)
-					return errors.New("OpenAI API error")
-				}
-				return nil
-			},
-		}
-
-		proxy.ServeHTTP(r.Response.Writer, r.Request)
-	})
+	s.BindHandler("/v1/chat/completions", service.ChatCompletionsProxy)
 }
--- modules/openai-proxy/service/service.go
+++ modules/openai-proxy/service/service.go
@@ -1,5 +1,61 @@
 package service
 
 import (
+	"errors"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"openai-proxy-backend/modules/openai-proxy/config"
 	_ "openai-proxy-backend/modules/openai-proxy/model"
+
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/os/gctx"
 )
+
+func ChatCompletionsProxy(r *ghttp.Request) {
+	ctx := gctx.New()
+
+	OpenaiProxyKeysService := NewOpenaiProxyKeysService()
+	// 检查用户token
+	token := r.Request.Header.Get("Authorization")
+	if token != "Bearer "+config.Config.Token {
+		r.Response.WriteJson(g.Map{
+			"code":    1001,
+			"message": "Key错误",
+		})
+		return
+	}
+
+	key, err := OpenaiProxyKeysService.RondomGetKey(ctx)
+	if err != nil {
+		r.Response.WriteJson(g.Map{
+			"code":    1001,
+			"message": "无可用OpenAI Key",
+		})
+		return
+	}
+	g.Log().Debug(ctx, "Current Key is %s", key)
+	u, _ := url.Parse(config.Config.OpenAIHost)
+	proxy := &httputil.ReverseProxy{Director: func(req *http.Request) {
+		req.URL.Scheme = u.Scheme
+		req.URL.Host = u.Host
+		req.Host = u.Host
+		req.Header.Set("Authorization", "Bearer "+key)
+	},
+		ModifyResponse: func(resp *http.Response) error {
+			if resp.StatusCode != http.StatusOK {
+				token := resp.Request.Header.Get("Authorization")
+				// g.Dump(token)
+				g.Log().Errorf(ctx, "Current Key is %s,  OpenAI API error: %s", token, resp.Status)
+				// 设置requestKey失效 requestKey为token 去掉Bearer 后面的
+				requestKey := token[7:]
+				OpenaiProxyKeysService.SetKeyInvalid(ctx, requestKey)
+				return errors.New("OpenAI API error")
+			}
+			return nil
+		},
+	}
+
+	proxy.ServeHTTP(r.Response.Writer, r.Request)
+}
